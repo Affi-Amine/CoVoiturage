1675cf01730da9b53600f3473e1e4a6704d3b1fb
Maynewi <affi.amin.work@gmail.com>
2023-08-04 15:24:31 +0100

    starting to work on the profile screen

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,5 +1,6 @@
 //import 'package:covoiturage/screens/authentication/signIn.dart';
 //import 'package:covoiturage/screens/authentication/welcome.dart';
+import 'package:covoiturage/screens/authentication/welcome.dart';
 import 'package:covoiturage/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -19,8 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
-
+      home: const HomeScreen(),
     );
   }
 }
